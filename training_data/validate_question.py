ec26b1c816322141836c3009a8f11741d5226ebe
Robert Mihail UNGUREANU <ungureanu.robert97@gmail.com>
2018-01-14 23:09:56 +0200

    Fixed issue with validate_question.py

--- training_data/validate_question.py
+++ training_data/validate_question.py
@@ -1,9 +1,12 @@
+import os
 import json
 import random
 
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+QUESTIONS_FILE_PATH = os.path.join(BASE_PATH, 'training_data', 'all_questions.json')
 
 def retrieve_questions(limit=1):
-    with open('all_questions.json', 'r', encoding = "ISO-8859-1") as handle:
+    with open(QUESTIONS_FILE_PATH, 'r', encoding = "ISO-8859-1") as handle:
         data = handle.read()
 
     questions = json.loads(data)
