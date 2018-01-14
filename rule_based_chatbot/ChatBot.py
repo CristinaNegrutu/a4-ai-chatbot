2c515f294964ce9f93e92062b22850ab4e641418
Cristina Negrutu <cristina.negrutu96@gmail.com>
2018-01-14 17:21:04 +0200

    i tried

--- rule_based_chatbot/ChatBot.py
+++ rule_based_chatbot/ChatBot.py
@@ -1,24 +1,47 @@
-import sys
-import os
-sys.path.append(r'.')
-os.chdir(r'aimls')
-import aiml
+import os, sys, inspect
 
-if __name__ == '__main__':
-    
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+
+def from_console():
+    sys.path.append(r'.')
+    os.chdir(r'aimls')
+    import aiml
     chat_bot = aiml.Kernel()
 
     chat_bot.learn('startup.xml')
 
     chat_bot.respond('load aiml b')
-    
+
     while True:
         user_input = input('You >')
         bot_response = chat_bot.respond(user_input)
-        
+
         if bot_response == "Sorry. I didn't quite get that.":
             # Use the neural network to generate a response
             pass
         else:
             print(bot_response)
 
+
+def from_ui(user_input):
+    from .aiml import Kernel
+    chat_bot = Kernel()
+
+    chat_bot.learn('startup.xml')
+
+    chat_bot.respond('load aiml b')
+
+    bot_response = chat_bot.respond(user_input)
+
+    if bot_response == "Sorry. I didn't quite get that.":
+        # Use the neural network to generate a response
+        pass
+    else:
+        return bot_response
+
+
+if __name__ == '__main__':
+    from_console()
--- ui/hello.py
+++ ui/hello.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template, request
 
+import os, sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from rule_based_chatbot import ChatBot
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-     return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+
+@app.route('/respond', methods=['POST'])
+def form_post():
+    print(os.path.dirname(__file__))
+    from pathlib import Path
+    aimls_dir = Path(r'../rule_based_chatbot/aimls')
+
+    if aimls_dir.exists():
+        os.chdir(r'../rule_based_chatbot/aimls')
+
     text = request.form['text']
+    response = "Chatbot: " + ChatBot.from_ui(text)
 
-    print(text)
-    return "Bot: I don't know the answer!"
+    return response
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 # run with:
-#	export FLASK_APP=hello.py
-#       flask run
+# export FLASK_APP=hello.py
+#    flask run
