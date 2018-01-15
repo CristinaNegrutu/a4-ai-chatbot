fd2ae8b89659b7ebe6dcc89f6effa34f274b537a
Cristina Negrutu <cristina.negrutu96@gmail.com>
2018-01-15 23:26:50 +0200

    biutiful

--- checked_answers/checked_answers.py
+++ checked_answers/checked_answers.py
@@ -1,16 +1,11 @@
 from NLP import TextProcessingWebScraping
 from . import similarity
 
-import os
-
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CHROMEDRIVER_PATH = os.path.join(BASE_PATH, 'NLP', 'chromedriver.exe')
-
 def raspuns(intrebare, raspunsTest, raspunsStudent):
     listaRaspunsTest = TextProcessingWebScraping.populate_lists(r"http://nlptools.info.uaic.ro/WebPosRo/", raspunsTest,
-                                                                CHROMEDRIVER_PATH)
+                                                                1)
     listaRaspunsStudent = TextProcessingWebScraping.populate_lists(r"http://nlptools.info.uaic.ro/WebPosRo/",
                                                                    raspunsStudent,
-                                                                   CHROMEDRIVER_PATH)
+                                                                   0)
 
     return similarity.similaritate(listaRaspunsTest, listaRaspunsStudent)
